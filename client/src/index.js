6fead15e308921c82ef8f65b928704f50efb4b12
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-12-21 23:43:20 +0530

    WIP - Frontend theme setUp

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+import globalreducer from "states"
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer:{
+    global:globalreducer,
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
