d16472788691d222b507dee5b8eaeda872a4bd8e
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-12-21 22:54:26 +0530

    WIP - react starter

--- server/index.js
+++ server/index.js
@@ -28,6 +28,13 @@ app.use("/general", generalRoutes)
 app.use("/management", managementRoutes)
 app.use("/sales", salesRoutes)
 
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    app.listen(PORT, ()=>console.log(`Server Port: ${PORT}`))
+}).catch((error)=>console.log(`${error} can not connect` ))
 
 
 
