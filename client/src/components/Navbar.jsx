ea00d720f5861bfde0954893ca66c3c5122ef711
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-12-24 12:33:37 +0530

    WIP-Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "./FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "states";
+import profileImage from "assets/profile.jpg"
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
