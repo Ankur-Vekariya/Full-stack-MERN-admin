89b08790cd6655b1f3f1049397765163350896a6
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-12-29 22:40:32 +0530

    WIP - Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,11 +11,28 @@ import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "states";
 import profileImage from "assets/profile.jpg";
-import { AppBar, IconButton, InputBase, Toolbar, useTheme } from "@mui/material";
+import {
+  Typography,
+  AppBar,
+  Button,
+  IconButton,
+  InputBase,
+  Toolbar,
+  useTheme,
+  Box,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import ProfileImage from "assets/profile.jpg";
 
-const Navbar = ({isSidebarOpen, setIsSidebarOpen}) => {
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleCLose = () => setAnchorEl(null);
 
   return (
     <AppBar
@@ -25,29 +42,83 @@ const Navbar = ({isSidebarOpen, setIsSidebarOpen}) => {
         boxShadow: "none",
       }}
     >
-      <Toolbar sx={{justifyContent:"space-between"}}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
         <FlexBetween>
-          <IconButton onClick={()=> setIsSidebarOpen(true)}>
+          <IconButton onClick={() => setIsSidebarOpen(true)}>
             <MenuIcon />
           </IconButton>
-          <FlexBetween backgroundColor={theme.palette.background.alt} borderRadius="9px" gap="3rem" p="0.1rem 1.5rem">
-            <InputBase placeholder="Search..."/>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
             <IconButton>
-              <Search/>
+              <Search />
             </IconButton>
-
           </FlexBetween>
         </FlexBetween>
         <FlexBetween gap="1.5rem">
-          <IconButton onClick={()=> dispatch(setMode())}>
+          <IconButton onClick={() => dispatch(setMode())}>
             {theme.palette.mode === "dark" ? (
-              <DarkModeOutlined sx={{fontSize:"25px"}}/> 
-            ): (<LightModeOutlined sx={{fontSize:"25px"}}/>)}
-
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
           </IconButton>
           <IconButton>
-            <SettingsOutlined/>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
           </IconButton>
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform: "none",
+                gap: "1rem",
+              }}
+            >
+              <Box
+                component="img"
+                alt="profile"
+                src={ProfileImage}
+                height="32px"
+                width="32px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.75rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleCLose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+              {/* <MenuItem onClick={handleClose}> */}
+              <MenuItem>LogOut</MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
