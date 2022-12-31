20cf81b1b7e0fb87ced43b43f0508d1761e652fb
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-12-31 20:39:00 +0530

    WIP - products page

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Card,
   CardActions,
-  CardContents,
+  CardContent,
   Collapse,
   Button,
   Typography,
@@ -14,12 +14,65 @@ import {
 import Header from "../../components/Header";
 import { useGetProductsQuery } from "states/api";
 
-const Products = () => {
-  const { data, isloading } = useGetProductsQuery();
-  console.log("data=============", data);
+const Product = (props) => {
+  const theme = useTheme();
+  const [isExpanded, setIsExpanded] = useState(false);
+  console.log("props============>>>>>>>", props);
   return (
-    <Box>
+    <Card
+      sx={{
+        backgroundImage: "none",
+        backgroundColor: theme.palette.background.alt,
+        borderRadius: "0.55rem",
+      }}
+    >
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 14 }}
+          color={theme.palette.background[700]}
+          gutterBottom
+        >
+          {props?.data?.category}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {props?.data?.name}
+        </Typography>
+        <Typography sx={{ mb: "1.5rem" }} color={theme.palette.background[700]}>
+          ${Number(props?.data?.price).toFixed(2)}
+        </Typography>
+        <Rating name="read-only" value={props?.data?.rating} readOnly />
+        <Typography variant="body2">{props?.data?.description}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+const Products = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const { data, isloading } = useGetProductsQuery();
+  console.log("data=============", JSON.stringify(data));
+  return (
+    <Box m="1.5rem 2.5rem">
       <Header title={"Products"} subTitle={"See Your Product List"} />
+      {data || !isloading ? (
+        <Box
+          mt="20px"
+          display="grid"
+          gridTemplateColumns="repeat(4,minmax(0,1fr))"
+          justifyContent="space-between"
+          rowGap="20px"
+          columnGap="1.33%"
+          sx={{
+            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+          }}
+        >
+          {data?.map((product, index) => (
+            <Product data={product} key={index} />
+          ))}
+        </Box>
+      ) : (
+        <>loadiing...</>
+      )}
     </Box>
   );
 };
